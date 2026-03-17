37fe517fd528126db56e56225ddd3633fe0cd4bd
Hugo Baizan <47147272+elhugobaizan@users.noreply.github.com>
2026-03-17 15:38:50 +0000

    habilitar CORS

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
